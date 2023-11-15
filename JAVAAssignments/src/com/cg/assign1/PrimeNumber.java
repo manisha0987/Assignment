28b30c8d5d7d913c90cef5404faa1a7b21c887c6
manisha0987 <149365681+manisha0987@users.noreply.github.com>
2023-11-15 20:42:53 +0530

    Add files via upload

---
+++ JAVAAssignments/src/com/cg/assign1/PrimeNumber.java
@@ -0,0 +1,25 @@
+package com.cg.assign1;
+
+		 
+		public class PrimeNumber {
+		 
+			public static void main(String[] args) {
+				int n=100;
+				for(int i=2;i<=n;i++) {
+					boolean flag=true;
+					for(int j=2;j<i;j++) {
+						if(i%j==0)
+							flag=false;
+					}
+					if(flag) {
+					System.out.println("Prime numbers are:"+i);	
+					}
+				}
+		 
+			}
+		 
+		
+
+	}
+
+
